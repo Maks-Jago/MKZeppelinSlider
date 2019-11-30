069ee3764db3a1e93e7870e0d29f1c7a65627a4a
Max Kuznetsov <mx.kuznetsov.dev@gmail.com>
2019-12-01 01:34:22 +0200

    add incline animation

---
+++ MKZeppelinSlider/MKZeppelinSlider/ViewModifiers/Inclinable.swift
@@ -0,0 +1,51 @@
+//
+//  Inclinable.swift
+//  MKZeppelinSlider
+//
+//  Created by Max Kuznetsov on 30.11.2019.
+//  Copyright © 2019 molfar.io. All rights reserved.
+//
+
+import SwiftUI
+
+enum InclineDirection: Double {
+    case none = 0
+    case leading = -30
+    case trailing = 30
+}
+
+extension View {
+
+    func incline(direction: InclineDirection) -> some View {
+        ModifiedContent(content: self, modifier: InclineEffect(inclineDirection: direction))
+    }
+}
+
+
+struct InclineEffect: GeometryEffect {
+
+    var degrees: Double
+    var animationValue: Double
+
+    init(inclineDirection: InclineDirection) {
+        degrees = inclineDirection.rawValue
+        animationValue = inclineDirection == .leading ? 1 : 0
+    }
+
+    var animatableData: Double {
+        get { animationValue }
+        set { animationValue = newValue }
+    }
+
+    func effectValue(size: CGSize) -> ProjectionTransform {
+        let angle = degrees * sin(animationValue * (.pi / 2.0))
+        print("\(angle)")
+
+//        let angle = CGFloat(degrees) * CGFloat(Double.pi / 180)
+        let affineTransform = CGAffineTransform(translationX: size.width*0.5, y: size.height*0.5)
+            .rotated(by: CGFloat(angle))
+            .translatedBy(x: -size.width*0.5, y: -size.height*0.5)
+
+        return ProjectionTransform(affineTransform)
+    }
+}
--- MKZeppelinSlider/MKZeppelinSlider/Views/MKZeppelinSlider.swift
+++ MKZeppelinSlider/MKZeppelinSlider/Views/MKZeppelinSlider.swift
@@ -7,20 +7,38 @@
 //
 
 import SwiftUI
+import Combine
 
 struct MKZeppelinSliderView: View {
     @Binding var animationValue: Double
+    var airshipAppearingDelay: Double = 0
+
     @State private var sliderTitle: String = "5"
     @State private var showAirship: Bool = false
+    @State private var inclineDirection: InclineDirection = .none
 
-    var airshipAppearingDelay: Double = 0
+    private let resetInclineSubject: PassthroughSubject<Void, Never> = .init()
+    private var resetInclinePublisher: AnyPublisher<InclineDirection, Never> {
+        resetInclineSubject
+            .debounce(for: 0.3, scheduler: RunLoop.main)
+            .map { InclineDirection.none }
+            .removeDuplicates()
+            .eraseToAnyPublisher()
+    }
 
     private var sliderBinding: Binding<Double> {
         .init(get: { () -> Double in
             self.animationValue
         }) { newValue in
             self.sliderTitle = "\(Int(newValue))"
+            let newInclineDirection: InclineDirection = newValue > self.animationValue ? .leading : .trailing
+
+            withAnimation {
+                self.inclineDirection = newInclineDirection
+            }
+
             self.animationValue = newValue
+            self.resetInclineSubject.send()
         }
     }
 
@@ -28,22 +46,30 @@ struct MKZeppelinSliderView: View {
         GeometryReader { proxy in
             VStack(alignment: .leading) {
                     ZStack {
-                        Spacer().aspectFit().frame(height: 89)
+                        Spacer().frame(height: 89)
                         if self.showAirship {
                             ZStack {
                                 Image.airship
                                     .aspectFit()
                                     .frame(width: 150)
                                     .layoutPriority(1)
+                                    .rotationEffect(.degrees(self.inclineDirection.rawValue))
+                                    .onReceive(self.resetInclinePublisher) { incline in
+                                        withAnimation {
+                                            self.inclineDirection = incline
+                                        }
+                                }
 
                                 Text("\(self.sliderTitle)")
                                     .font(Font.largeTitle.bold())
                                     .foregroundColor(Color.white)
                                     .offset(x: -20, y: -5)
-                            }.transition(AnyTransition.scale.combined(with: .move(edge: .bottom)))
+                                    .rotationEffect(.degrees(self.inclineDirection.rawValue))
+                            }
+                            .transition(AnyTransition.scale.combined(with: .move(edge: .bottom)))
                         }
                     }
-                    .padding(.bottom, 20)
+                    .padding(.bottom, 30)
                     .offset(x: CGFloat(self.animationValue) * (proxy.size.width / 20.0) - 55)
 
                 Slider(value: self.sliderBinding, in: 0...20, step: 1)
@@ -64,12 +90,13 @@ struct MKZeppelinSlider_Previews: PreviewProvider {
     static var values: [Int] = Array(0...10)
 
     static var previews: some View {
-        Group {
-            ForEach(values, id: \.self) { value in
-                MKZeppelinSliderView(animationValue: .constant(Double(value * 2)))
-            }
-        }
-        .previewLayout(.fixed(width: 500, height: 500))
+//        Group {
+//            ForEach(values, id: \.self) { value in
+//                MKZeppelinSliderView(animationValue: .constant(Double(value * 2)))
+//            }
+//        }
+        MKZeppelinSliderView(animationValue: .constant(10))
+            .previewLayout(.fixed(width: 500, height: 500))
     }
 }
 
